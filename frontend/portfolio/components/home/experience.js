8e1da68d20ab02e27fe5a094a57548629a8796c9
Joel Helbling <helblingjoel@gmail.com>
2022-02-09 23:11:39 +0000

    Latest post

--- frontend/portfolio/components/home/experience.js
+++ frontend/portfolio/components/home/experience.js
@@ -2,114 +2,64 @@ export default function Experience() {
     return (
         <div className="experience pt-6">
           <p className="title">Experience</p>
+            <p className="title is-5">Work History</p>
 
-          <div className="columns">
-            <div className="column">
-              <p className="title is-5">Work History</p>
-
-              {/* NHSD */}
-              <div className="card">
-                <header className="card-header">
-                  <p className="card-header-title" onClick={toggleNHSD}>
-                    NHS Digital
-                  </p>
-                  <button className="card-header-icon" onClick={toggleNHSD} aria-label="more options">
-                    <span className="icon">
-                      <p>▽</p>
-                    </span>
-                  </button>
-                </header>
-                <div className="card-content hidden" id="nhsdContent">
-                  <p><b>Graduate Scheme</b></p>
-                  <p>Pathways - Developer</p>
-                </div>
+            {/* NHSD */}
+            <div className="card is-fullwidth">
+              <header className="card-header">
+                <p className="card-header-title" onClick={toggleNHSD}>
+                  NHS Digital
+                </p>
+                <button className="card-header-icon" onClick={toggleNHSD} aria-label="more options">
+                  <span className="icon">
+                    <p>▽</p>
+                  </span>
+                </button>
+              </header>
+              <div className="card-content hidden" id="nhsdContent">
+                <p><b>Graduate Scheme</b></p>
+                <p>Pathways - Developer</p>
+                <p>C#, JS, CloudOps (AWS)</p>
               </div>
-
-              {/* Waitrose */}
-              <div className="card mt-2">
-                <header className="card-header">
-                  <p className="card-header-title" onClick={toggleWaitrose}>
-                    Waitrose
-                  </p>
-                  <button className="card-header-icon" onClick={toggleWaitrose} aria-label="more options">
-                    <span className="icon">
-                      <p>▽</p>
-                    </span>
-                  </button>
-                </header>
-                <div className="card-content hidden" id="waitroseContent">
-                  <p><b>Legalities</b></p>
-                  <p>Pathways - Developer</p>
-                  <p><b>Cashier</b></p>
-                  <p>Pathways - Developer</p>
-                </div>
-              </div>
-
-              {/* McDonalds */}
-              <div className="card mt-2">
-                <header className="card-header">
-                  <p className="card-header-title" onClick={toggleMcDonalds}>
-                    McDonalds
-                  </p>
-                  <button className="card-header-icon" onClick={toggleMcDonalds} aria-label="more options">
-                    <span className="icon">
-                      <p>▽</p>
-                    </span>
-                  </button>
-                </header>
-                <div className="card-content hidden" id="mcdonaldsContent">
-                  <p>Crew Member</p>
-                </div>
-              </div>
-
             </div>
 
-            <div className="column">
-              <p className="title is-5">Education</p>
-
-              {/* University of Kent */}
-              <div className="card">
-                <header className="card-header">
-                  <p className="card-header-title" onClick={toggleUniversity}>
-                    University of Kent
-                  </p>
-                  <button className="card-header-icon" onClick={toggleUniversity} aria-label="more options">
-                    <span className="icon">
-                      <p>▽</p>
-                    </span>
-                  </button>
-                </header>
-                
-                <div className="card-content hidden" id="universityContent">
-                  <p><b>BSc Computer Science: Distinction</b></p>
-                  <a className="mt-3 mb-3" href="/documents/university_transcript.pdf">Transcript</a>
-                  <p>In my final year I worked with KITC - an in-house consultancy firm supporting local businesses' online presence during the height of the pandemic</p>
-                </div>
-
+            {/* Waitrose */}
+            <div className="card mt-2">
+              <header className="card-header">
+                <p className="card-header-title" onClick={toggleWaitrose}>
+                  Waitrose
+                </p>
+                <button className="card-header-icon" onClick={toggleWaitrose} aria-label="more options">
+                  <span className="icon">
+                    <p>▽</p>
+                  </span>
+                </button>
+              </header>
+              <div className="card-content hidden" id="waitroseContent">
+                <p><b>Legalities</b></p>
+                <p>Working in a small team with ever changing environments.</p>
+                <p><b>Cashier</b></p>
+                <p>Customer service</p>
               </div>
-
-              {/* Royal Harbour Academy */}
-              <div className="card mt-2">
-                <header className="card-header">
-                  <p className="card-header-title" onClick={toggleSixthForm}>
-                    Royal Harbour Academy
-                  </p>
-                  <button className="card-header-icon" onClick={toggleSixthForm} aria-label="more options">
-                    <span className="icon">
-                      <p>▽</p>
-                    </span>
-                  </button>
-                </header>
-                
-                <div className="card-content hidden" id="sixthformContent">
-                  <p>Btec Public Services: D*DD</p>
-                  <p>IB Math Studies: 5/7</p>
-                  <p>IB ITGS: 6/7</p>
-                </div>
-              </div>
-
             </div>
-          </div>
+
+            {/* McDonalds */}
+            <div className="card mt-2">
+              <header className="card-header">
+                <p className="card-header-title" onClick={toggleMcDonalds}>
+                  McDonalds
+                </p>
+                <button className="card-header-icon" onClick={toggleMcDonalds} aria-label="more options">
+                  <span className="icon">
+                    <p>▽</p>
+                  </span>
+                </button>
+              </header>
+              <div className="card-content hidden" id="mcdonaldsContent">
+                <p><b>Crew Member</b></p>
+                <p>Mostly in customer-service centric roles, especially in the high pressure DriveThru environment in two different stores.</p>
+              </div>
+            </div>
         </div>
     );
 }
@@ -124,12 +74,4 @@ const toggleWaitrose = () => {
 
 const toggleMcDonalds = () => {
   document.querySelector("#mcdonaldsContent").classList.toggle("hidden");
-}
-
-const toggleUniversity = () => {
-  document.querySelector("#universityContent").classList.toggle("hidden");
-}
-
-const toggleSixthForm = () => {
-  document.querySelector("#sixthformContent").classList.toggle("hidden");
 }
--- frontend/portfolio/components/home/latestPost.js
+++ frontend/portfolio/components/home/latestPost.js
@@ -5,16 +5,20 @@ export default function LatestPost() {
 
             <div className="card mt-2">
                 <header className="card-header">
-                  <p className="card-header-title">Latest Post</p>
+                  <p className="card-header-title title is-5" id="latestBlogTitle">{postTitle}</p>
                   <div className="has-text-right pt-2 pr-3">
                     <i><time dateTime="21/06/2022">21/06/2022</time></i>
                   </div>
                 </header>
                 <div className="card-content" id="mcdonaldsContent">
-                  <p>Content Preview</p>
+                  <p>{postPreview}</p>
                 </div>
                
               </div>
         </div>
     )
 }
+
+const postTitle = `Portfolio`;
+
+const postPreview = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nisl sed nulla mattis lobortis. Vivamus eu condimentum sem. Quisque pretium orci diam, ut euismod turpis iaculis id. Phasellus fermentum. `
