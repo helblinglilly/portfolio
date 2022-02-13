d9a6a90d1e07022deb6ecd7cc9b8f9f3b6e4e7f9
Joel Helbling <helblingjoel@gmail.com>
2022-02-13 00:31:02 +0000

    Maybe not rewrite all requests to twitter

--- frontend/portfolio/next.config.js
+++ frontend/portfolio/next.config.js
@@ -2,12 +2,12 @@ module.exports = {
 	reactStrictMode: true,
 	locales: ["en-GB"],
 	defaultLocale: "en-GB",
-	async rewrites() {
-		return [
-			{
-				source: "/:path*",
-				destination: "https://api.twitter.com/:path*",
-			},
-		];
-	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: "/:path*",
+	// 			destination: "https://api.twitter.com/:path*",
+	// 		},
+	// 	];
+	// },
 };
