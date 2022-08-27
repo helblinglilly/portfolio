51334f841aa237f45ec29da6a8e8825d038784bb
Joel Helbling <helblingjoel@gmail.com>
2022-08-27 19:29:27 +0100

    Fixed syntax

--- vercel.sh
+++ vercel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "VERCEL_ENV: $VERCEL_ENV"
 
-if "$VERCEL_ENV" == "production"; then
+if [ "$VERCEL_ENV" == "production" ]; then
   echo "✅ - Build can proceed"
   exit 1;
-elif  "$VERCEL_ENV" == "preview"; then
+elif [ "$VERCEL_ENV" == "preview" ]; then
   echo "✅ - Build can proceed"
   exit 1;
 else
