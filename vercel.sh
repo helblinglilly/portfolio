98b55d30d44daddcff88d336b15096092e1fc0cf
Joel Helbling <helblingjoel@gmail.com>
2022-08-27 19:31:46 +0100

    Use git branch name

--- vercel.sh
+++ vercel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [ "$VERCEL_ENV" == "production" ]; then
+if [ "$VERCEL_GIT_COMMIT_REF" == "main" ]; then
   echo "✅ - Build can proceed"
   exit 1;
-elif [ "$VERCEL_ENV" == "preview" ]; then
+elif [ "$VERCEL_GIT_COMMIT_REF" == "develop" ]; then
   echo "✅ - Build can proceed"
   exit 1;
 else
