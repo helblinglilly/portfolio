1e7c2aa238a2c77a3b8ac14d62edb7e396c9119b
Joel Helbling <helblingjoel@gmail.com>
2022-11-30 13:30:19 +0000

    Remove highlightjs from every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,7 @@
 import "../styles/globals.css";
-import "highlight.js/styles/monokai-sublime.css";
-import hljs from "highlight.js";
-import { useEffect } from "react";
-
 import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
-	useEffect(() => {
-		hljs.highlightAll();
-	}, []);
 	return (
 		<ThemeProvider defaultTheme="system">
 			<Component {...pageProps} />
