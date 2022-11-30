5a051c72ebedb17b8093051ceba48a522af279a2
Joel Helbling <helblingjoel@gmail.com>
2022-11-30 13:30:30 +0000

    Don't import all of react

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Layout from "../../Layouts/Layout";
 import SocialPreview from "../../components/SocialPreview";
 import axios, { AxiosError } from "axios";
