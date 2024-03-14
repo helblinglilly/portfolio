1dd93685966868184dd17d3dd4c9de220393ad00
Lilly Helbling <lilly@helbling.uk>
2024-03-14 23:00:51 +0000

    Tidy configs

--- next.config.js
+++ next.config.js
@@ -1,25 +1,31 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-	enabled: process.env.ANALYZE === "true",
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 });
 
 module.exports = withBundleAnalyzer({
-	reactStrictMode: true,
-	devIndicators: {
-		buildActivity: false,
-	},
-	images: {
-		domains: [
-			"opengraph.githubassets.com",
-			"github-readme-stats.vercel.app",
-			"pokecompanion.helbling.uk",
-			"pokemon.helbling.uk",
-			"www.sweetaf.uk",
-		],
-	},
+  reactStrictMode: true,
+  devIndicators: {
+    buildActivity: false,
+  },
+  images: {
+    domains: [
+      'opengraph.githubassets.com',
+      'github-readme-stats.vercel.app',
+      'pokecompanion.helbling.uk',
+      'pokemon.helbling.uk',
+      'www.sweetaf.uk',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.helbling.uk',
+      },
+    ],
+  },
 
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
