8db27c2c0c82dc903272add6f22c9d2163e3d756
Lilly Helbling <lilly@helbling.uk>
2024-03-18 18:59:46 +0000

    Add redirects for legacy links

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,13 @@ export function middleware(request: NextRequest) {
   if (request.url.endsWith('/blog/2022')) return NextResponse.rewrite(new URL('/blog', request.url));
   if (request.url.endsWith('/blog/2023')) return NextResponse.rewrite(new URL('/blog', request.url));
 
-  return undefined;
+  // Redirects for old blog posts
+  if (request.url.endsWith('/blog/2023/cloudflare')) return NextResponse.rewrite(new URL('/blog/2023-cloudflare', request.url));
+  if (request.url.endsWith('/blog/2023/pokemon')) return NextResponse.rewrite(new URL('/blog/2023-pokewiki', request.url));
+  if (request.url.endsWith('/blog/2022/vercel')) return NextResponse.rewrite(new URL('/blog/2022-vercel', request.url));
+  if (request.url.endsWith('/blog/2022/homeserver-pi')) return NextResponse.rewrite(new URL('/blog/2022-homeserver', request.url));
+  if (request.url.endsWith('/blog/2022/aws-summit')) return NextResponse.rewrite(new URL('/blog/2022-aws-summit', request.url));
+  if (request.url.endsWith('/blog/2022/r-has-a-problem')) return NextResponse.rewrite(new URL('/blog/2022-r-rant', request.url));
+
+  return NextResponse;
 }
