9717ff22200f344065f15a93991875a948329768
Lilly Helbling <lilly@helbling.uk>
2025-02-13 23:24:47 +0000

    Add more accent colours

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -27,12 +27,12 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={
-        "flex flex-1 items-center justify-between py-4 text-md font-medium transition-all hover:underline text-left [&[data-state=open]>svg]:rotate-180"
+        "flex flex-1 items-center justify-between py-4 text-md font-medium transition-[transform, text-decoration-color] hover:underline hover:decoration-[#F714B1] text-left [&[data-state=open]>svg]:rotate-180"
       }
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200 text-[#F714B1]" />
+      <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
