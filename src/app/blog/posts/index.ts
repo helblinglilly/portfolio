45f7987d99ade0deddef7693157ffd09fae27b12
Lilly Helbling <lilly@helbling.uk>
2025-05-01 23:35:05 +0100

    Split posts up into two Grammarly

--- src/app/blog/posts/index.ts
+++ src/app/blog/posts/index.ts
@@ -6,7 +6,7 @@ import * as Rant2022 from './2022-r-rant.mdx';
 import * as Vercel2022 from './2022-vercel.mdx';
 import * as Cloudflare2023 from './2023-cloudflare.mdx';
 import * as Pokewiki2023 from './2023-pokewiki.mdx';
-import * as TransRights2025 from './2025-trans-rights.mdx';
+import * as TransRights2025 from './2025-single-sex-speces-do-not-work.mdx';
 
 const AllPosts: IPostMetadata[] = [
   Rant2022,
@@ -15,6 +15,7 @@ const AllPosts: IPostMetadata[] = [
   Vercel2022,
   Cloudflare2023,
   Pokewiki2023,
+  // Democracy2025, // Do not advertise this without the context of the trans post
   TransRights2025,
 ].sort((a, b) => (new Date(a.publishedTime).valueOf() < new Date(b.publishedTime).valueOf()
   ? 1
