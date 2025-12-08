931adaa79a27318d3b279e19c042fe42f0b0fdd6
Lilly Helbling <lilly.helbling@onthebeach.co.uk>
2025-12-08 18:35:55 +0000

    Rearrange social media icons
    @FabioTavernini :eyes:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,37 +7,40 @@ import Script from "next/script";
 import { type ReactNode } from "react";
 import ClientLayout from "./clientLayout";
 import "./globals.css";
+import BlueSkyIcon from "@/components/atoms/Icons/BlueSky";
+import GithubIcon from "@/components/atoms/Icons/Github";
+import InstagramIcon from "@/components/atoms/Icons/Instagram";
+import LinkedinIcon from "@/components/atoms/Icons/LinkedIn";
 
 export function generateMetadata(): Metadata {
-	return metadataGenerator({
-		url: "https://helbling.uk",
-		publishedTime: new Date("2020-01-01").toISOString(),
-		modifiedTime: new Date("2024-10-06").toISOString(),
-	});
+  return metadataGenerator({
+    url: "https://helbling.uk",
+    publishedTime: new Date("2020-01-01").toISOString(),
+    modifiedTime: new Date("2024-10-06").toISOString(),
+  });
 }
 
 export default async function RootLayout({
-	children,
+  children,
 }: {
-	children: ReactNode;
+  children: ReactNode;
 }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="canonical" href="https://helbling.uk" />
+      </Head>
+      <body>
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <Script
+              src="https://analytics.helbling.uk/script.js"
+              data-website-id="11bbfb52-ea1f-401b-96ca-72f074fb41d1"
+              strategy="afterInteractive"
+            />
 
-	return (
-		<html lang="en" suppressHydrationWarning>
-		<Head>
-		  <link rel="canonical" href="https://helbling.uk" />
-		</Head>
-			<body>
-				{process.env.NODE_ENV === "production" && (
-				<>
-				  <Script
-            src="https://analytics.helbling.uk/script.js"
-            data-website-id="11bbfb52-ea1f-401b-96ca-72f074fb41d1"
-            strategy="afterInteractive"
-          />
-
-          <Script id="outbound-link-tracking" strategy="afterInteractive">
-            {`
+            <Script id="outbound-link-tracking" strategy="afterInteractive">
+              {`
               (() => {
                 const name = 'outbound-link-click';
                 document.querySelectorAll('a').forEach(a => {
@@ -48,15 +51,53 @@ export default async function RootLayout({
                 });
               })();
             `}
-          </Script>
-				</>
-				)}
-				<Theme>
-					<ClientLayout>
-						{children}
-					</ClientLayout>
-				</Theme>
-			</body>
-		</html>
-	);
+            </Script>
+          </>
+        )}
+        <Theme>
+          <ClientLayout>
+            <div className="grid gap-4">
+              {children}
+              <footer className="justify-around pt-4">
+                <section
+                  className="w-full flex justify-center gap-4"
+                  aria-label="Socials"
+                >
+                  <a
+                    href="https://github.com/helblinglilly"
+                    className="inline-flex gap-2 text-center link"
+                  >
+                    <GithubIcon width="25" height="25" />
+                  </a>
+
+                  <a
+                    href="https://bsky.app/profile/helbling.uk"
+                    className="inline-flex gap-2 text-center link"
+                  >
+                    <figure aria-label="BlueSky">
+                      <BlueSkyIcon width="25" height="25" />
+                    </figure>
+                  </a>
+
+                  <a
+                    href="https://instagram.com/helblinglilly"
+                    className="inline-flex gap-2 text-center link"
+                  >
+                    <InstagramIcon width="25" height="25" />
+                  </a>
+
+                  <a
+                    href="https://www.linkedin.com/in/lilly-helbling-707ba0171"
+                    className="inline-flex gap-2 text-center link"
+                  >
+                    <LinkedinIcon height="25" width="25" />
+                  </a>
+                </section>
+              </footer>
+            </div>
+          </ClientLayout>
+        </Theme>
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,13 @@ import "./homepage.css";
 import Intro from "@/homepage/intro";
 import Current from "@/homepage/current";
 import Tools from "@/homepage/tools";
-import GithubIcon from "@/components/atoms/Icons/Github";
-import BlueSkyIcon from "@/components/atoms/Icons/BlueSky";
-import LinkedinIcon from "@/components/atoms/Icons/LinkedIn";
-import InstgramIcon from "@/components/atoms/Icons/Instagram";
 
 const profilePictureURL = "https://static.helbling.uk/profile/current.jpg";
 
 function Homepage() {
   return (
     <React.Fragment>
-      <header className="flex flex-row gap-4 w-full justify-center pb-4">
+      <header className="flex flex-row gap-4 w-full justify-center">
         <Image
           src={profilePictureURL}
           alt="Profile Picture"
@@ -56,63 +52,6 @@ function Homepage() {
           <Tools />
         </article>
       </main>
-
-      <aside
-        className="grid w-full gap-2 pt-4 md:mx-auto md:fixed md:justify-end md:top-[5rem] md:pr-10"
-        aria-label="Social media links"
-      >
-        <h3 className="text-2xl font-semibold">Socials</h3>
-
-        <div className="grid grid-cols-4 md:grid-cols-1">
-          <div className="col-span-2 sm:col-span-1">
-            <a
-              href="https://github.com/helblinglilly"
-              className="inline-flex gap-2 text-center link"
-            >
-              <div className="w-[24px] h-auto grid justify-center">
-                <GithubIcon />
-              </div>
-              <p className="my-auto">Github</p>
-            </a>
-          </div>
-
-          <div className="col-span-2 sm:col-span-1">
-            <a
-              href="https://bsky.app/profile/helbling.uk"
-              className="inline-flex gap-2 text-center link"
-            >
-              <div className="w-[24px] h-auto grid justify-center">
-                <BlueSkyIcon />
-              </div>
-              <p className="my-auto">BlueSky</p>
-            </a>
-          </div>
-
-          <div className="col-span-2 sm:col-span-1">
-            <a
-              href="https://instagram.com/helblinglilly"
-              className="inline-flex gap-2 text-center link"
-            >
-              <div className="w-[24px] h-auto grid justify-center">
-                <InstgramIcon />
-              </div>
-              <p className="my-auto">Instagram</p>
-            </a>
-          </div>
-
-          <div className="col-span-2 sm:col-span-1">
-            <a
-              href="https://www.linkedin.com/in/lilly-helbling-707ba0171"
-              className="inline-flex gap-2 text-center link"
-            >
-              <div className="w-[24px] h-auto grid justify-center">
-                <LinkedinIcon />
-              </div>
-              <p className="my-auto">LinkedIn</p>
-            </a>
-          </div>
-        </div>
-      </aside>
     </React.Fragment>
   );
 }
