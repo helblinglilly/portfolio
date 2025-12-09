74922819bb4ab0d78feb3479411964a30fddb598
Lilly Helbling <lilly.helbling@onthebeach.co.uk>
2025-12-09 07:36:01 +0000

    Tweak intro copy

--- src/homepage/intro/index.tsx
+++ src/homepage/intro/index.tsx
@@ -1,10 +1,15 @@
-import React from "react"
+import React from "react";
 
-export default function IntroProfessional(){
+export default function IntroProfessional() {
   return (
     <React.Fragment>
       <p>I'm a Software Engineer based in Leeds, UK.</p>
-      <p>With a general interest in all things tech I offer an open, curious mind that's quick to adapt to new technologies. I'm working to become great at solving customer problems on the web and making sure I consider every part of the process.</p>
+      <p>
+        With a general interest in all things tech I offer an open, curious mind
+        that's quick to adapt to new technologies. I'm always honing my ability
+        to solve customer problems up and down the stack, making sure I consider
+        every part of the process.
+      </p>
     </React.Fragment>
-  )
+  );
 }
