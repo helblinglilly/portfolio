cc491c7f7adcd30e047f93c4613d70090ea8d5e6
Lilly Helbling <lilly.helbling@onthebeach.co.uk>
2026-09-24 11:54:59 +0100

    Update home contents

--- src/homepage/current/index.tsx
+++ src/homepage/current/index.tsx
@@ -19,25 +19,36 @@ export default function CurrentProfessional() {
         </div>
       </div>
 
+      <h2 className="h2 pb-2">Experience & Projects</h2>
+
       <div className="grid gap-2 pb-4">
         <h3 className="h3 font-bold text-lg">
-          On the Beach<span className="ml-2 text-sm">2023 - present</span>
+          Pokécompanion<span className="ml-2 text-sm">2021 - present</span>
+        </h3>
+        <p>
+          Active side project with a <a href="https://pokecompanion.com" className="link" target="_blank">website</a> and app on <a href="https://play.google.com/store/apps/details?id=com.helblinglilly.pokecompanion" className="link">Google Play</a>.
+        </p>
+      </div>
+
+      <div className="grid gap-2 pb-4">
+        <h3 className="h3 font-bold text-lg">
+          On the Beach<span className="ml-2 text-sm">2023 - 2026</span>
         </h3>
 
         <ul className="grid gap-2 border-s border-[#F714B1] relative list-none ml-3 [&>li]:list-none">
-          <li className="mt-2 mb-2 ms-4 -start-1.5">
+          <li className="mt-2 ms-4 -start-1.5">
             <div
               style={{ insetInlineStart: "-0.4rem" }}
               className="absolute mt-1.5 h-3 w-3 rounded-full bg-[#F714B1] animate-pulse"
             />
-            Promotion to Senior Software Engineer
+            Reduce various team's delivery times via AI Dev Enablement team
           </li>
           <li className="mt-2 mb-2 ms-4 -start-1.5">
             <div
               style={{ insetInlineStart: "-0.4rem" }}
               className="absolute mt-1.5 h-3 w-3 rounded-full bg-[#F714B1] animate-pulse"
             />
-            Decreased feedback delay from weeks to minutes by introducing a Backend for Frontend to the app
+            Architected and lead delivery of a Server-Driven UI framework delivered via a Backend for Frontend to the app
           </li>
           <li className="mb-2 ms-4 -start-1.5">
             <div
@@ -45,13 +56,6 @@ export default function CurrentProfessional() {
               className="absolute mt-1.5 h-3 w-3 rounded-full bg-[#F714B1]"
             />
             Team switch to the app team, increasing MAUs by 25% YOY
-          </li>
-          <li className="mb-2 ms-4 -start-1.5">
-            <div
-              style={{ insetInlineStart: "-0.4rem" }}
-              className="absolute mt-1.5 h-3 w-3 rounded-full bg-[#F714B1]"
-            />
-            Promotion to Software Engineer (Mid)
           </li>
           <li className="mb-2 ms-4 -start-1.5">
             <div
@@ -69,7 +73,7 @@ export default function CurrentProfessional() {
               style={{ insetInlineStart: "-0.4rem" }}
               className="absolute mt-1.5 h-3 w-3 rounded-full bg-[#F714B1]"
             />
-            1 year stint in the booking funnel
+            Run experiments and improvements in our eCommerce funnel
           </li>
         </ul>
       </div>
@@ -78,9 +82,9 @@ export default function CurrentProfessional() {
         NHS Digital <span className="ml-2 text-sm">2021 - 2023</span>
       </h3>
       <p>
-        Joined on a graduate scheme working in the Pathways team. Maintained
-        several .NET applications and supported our AWS migration.
+        Maintained several .NET web and desktop applications and supported our AWS migration.
       </p>
+
     </React.Fragment>
   );
 }
